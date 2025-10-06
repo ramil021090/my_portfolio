fccf1411bf79efb19062d3aabf88cfd3a9d127b7
ramil villahermosa <villahermosaramil2019@gmail.com>
2025-10-06 11:45:51 +0800

    update on text color

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './ProfilePic.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./ProfilePic.module.css";
 export default function ProfilePic() {
   return (
     <div>
-        <Link>
-    <img src="/profilePic.jpg" alt="profilePic" className={styles.profile}/>
-  </Link>
+      <Link>
+        <img
+          src="/profilePic.jpg"
+          alt="profilePic"
+          className={styles.profile}
+        />
+      </Link>
     </div>
-  )
+  );
 }
